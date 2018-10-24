134637d8efdcbca3f64bda72245632aa4fb3cb0e
naregkhodanian <nkhodanian@gmail.com>
2018-10-23 19:46:18 -0700

    Trying to fix FTP issues

--- CreateFTP.php
+++ CreateFTP.php
@@ -19,8 +19,12 @@ class CreateFTP {
         include('ssh/Net/SSH2.php');
         $ssh = new Net_SSH2('172.16.33.106');
         $ssh->login('root', 'portfolio4290') or die ("Login failed");
-        $directory = '/var/www/html/nareg';
-        echo $ssh->exec("command");
+        $directory = '../var/www/html/test3';
+        $ssh->exec('mkdir '. $directory);
+        $ssh->exec("useradd -M test3");
+        $ssh->exec('echo "test3:test" | sudo chpasswd');
+        $ssh->exec('pkill -u test3' );
+        $ssh->exec('chown -R test3 '. $directory);
     }
 
     function connectSSH(){
