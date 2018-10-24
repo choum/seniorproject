ba6033cf9a15f1513888366e2b761c3126dea633
Heather <dylanchoum123@yahoo.com>
2018-10-23 20:40:02 -0700

    csrf tokens in place on forms.... also worked a bit on login controller, need db functions for login and register and returning user type

--- login-controller.php
+++ login-controller.php
@@ -5,21 +5,44 @@ $post = hPost('action');
 switch ($post) {
   case 'login':
   //check to see form tokens are equal
-
-  //validate via DB
-  //take to correct dashboard
+  if (csrf_token_is_valid()) {
+    //validate via DB and get user type
+    if () {
+      //redirect to student
+    } else if () {
+      //redirect to instructor
+    } else if () {
+      //redirect to admin
+    } else {
+      include 'login.html';
+      end_session();
+    }
+  }
   break;
 
   case 'register':
   //check to form tokens
-  //validate inputs
-  //store in db
+  if (csrf_token_is_valid()) {
+    //sanitize user/password
+    $sUser = hPOST("username");
+    $sPass = hPOST("password");
+    $sFirst = hPOST("firstname");
+    $sLast = hPOST("lastname");
+    //sanitize Profile
+    $sResume = hPOST("resume");
+    $sWebsite = hPOST("website");
+    $sAbout = hPOST("about");
+
+    //validate
+    //store in db
+  }
 
   //take to dashboard
   include 'dashboard.html';
   break;
 
   default:
+  end_session();
   //end session
   include 'login.html';
 }
