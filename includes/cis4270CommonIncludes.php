96efc3ec042cbe8fb67c5eca30f41e4a383bb35e
Heather <dylanchoum123@yahoo.com>
2018-10-29 19:20:38 -0700

    fixed common includes

--- includes/cis4270CommonIncludes.php
+++ includes/cis4270CommonIncludes.php
@@ -3,11 +3,10 @@
 // @author jam
 // @version 180426
 
-include_once(NON_WEB_BASE_DIR .'common/includes/validationFunctions.php');
-include_once(NON_WEB_BASE_DIR .'common/includes/sanitizationFunctions.php');
-include_once(NON_WEB_BASE_DIR .'common/includes/csrf_token_functions.php');
-include_once(NON_WEB_BASE_DIR .'common/includes/general_functions.php');
-include_once(NON_WEB_BASE_DIR .'common/includes/request_forgery_functions.php');
-include_once(NON_WEB_BASE_DIR .'common/includes/session_hijacking_functions.php');
-include_once(NON_WEB_BASE_DIR .'common/includes/csrf_request_type_functions.php');
-
+include_once('validationFunctions.php');
+include_once('sanitizationFunctions.php');
+include_once('csrf_token_functions.php');
+include_once('general_functions.php');
+include_once('request_forgery_functions.php');
+include_once('session_hijacking_functions.php');
+include_once('common/includes/csrf_request_type_functions.php');
