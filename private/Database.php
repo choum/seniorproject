a2b22d614497ea89d5df62525587c3c8b04fe28e
jcrest1993 <35124068+jcrest1993@users.noreply.github.com>
2018-10-31 12:46:30 -0700

    Error Messages working again
    Adding a user should now throw specific errors when it is a duplicate username or null entry when not allowed.

--- private/Database.php
+++ private/Database.php
@@ -30,6 +30,7 @@
 
                 //Create connection
                 $this->db = new  PDO($dsn, $username, $password);
+                $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException  $e) {
 
                 $error_message = $e->getMessage();
