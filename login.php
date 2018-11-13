a9631f8bcae8c9ad07a5d68ef7f451891652eb0f
Nat Rivera <nat@natrivera.com>
2018-11-12 19:20:24 -0800

    Add profiles to login page

--- login.php
+++ login.php
@@ -37,7 +37,7 @@ create_csrf_token();
 </head>
 
 <body>
-  <div class="main a">
+  <div class="main a" style='min-height: 100vh;'>
     <div class="card">
       <div class="card-body">
         <h3 class="card-title">Login</h3>
@@ -64,7 +64,69 @@ create_csrf_token();
         </form>
       </div>
     </div>
+    <div style='position: absolute; bottom: 0; text-align: center;'>
+      <a href='#team'><button id='dev' class='btn'>Meet the Development Team</button></a>
+    </div>
   </div>
+  <div id='team' style='min-height: 100vh;' >
+    <div class='title'>
+      <h3>Meet the Development Team</h3>
+    </div>
+    <div class='row'>
+      <div class='col-xs-12 col-sm-6 col-xl-3'>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="https://www.cornwallbusinessawards.co.uk/wp-content/uploads/2017/11/dummy450x450.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h3 class="card-title">Nat Rivera</h3>
+            <h5>Team Lead / Front-End Developer</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="https://www.linkedin.com/in/natrivera/" class="btn btn-primary" target='_blank'>LinkedIn</a>
+          </div>
+        </div>
+      </div>
+      <div class='col-xs-12 col-sm-6 col-xl-3'>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="https://www.cornwallbusinessawards.co.uk/wp-content/uploads/2017/11/dummy450x450.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h3 class="card-title">Heather Tran</h3>
+            <h5>Scrumaster / Front-End Developer</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary" target='_blank'>LinkedIn</a>
+          </div>
+        </div>
+      </div>
+      <div class='col-xs-12 col-sm-6 col-xl-3'>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="https://www.cornwallbusinessawards.co.uk/wp-content/uploads/2017/11/dummy450x450.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h3 class="card-title">Justin Crest</h3>
+            <h5>Database Developer / Quality Assurance</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary" target='_blank'>LinkedIn</a>
+          </div>
+        </div>
+      </div>
+      <div class='col-xs-12 col-sm-6 col-xl-3'>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="https://www.cornwallbusinessawards.co.uk/wp-content/uploads/2017/11/dummy450x450.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h3 class="card-title">Nareg Khodanian</h3>
+            <h5>Back-End Developer / Hardware Specialist</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary" target='_blank'>LinkedIn</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    $('#dev').click(function(){
+      $('html, body').animate({
+        scrollTop: $('#team').offset().top
+      }, 200);
+    });
+  </script>
 </body>
 
 </html>
