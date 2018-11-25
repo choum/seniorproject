8dd12a7564fb2595a085acc927839cf53ce02cfc
Heather <dylanchoum123@yahoo.com>
2018-11-25 12:29:54 -0800

    image upload that probably doesn't work but i tried

--- login-functions.php
+++ login-functions.php
@@ -128,7 +128,11 @@ function register() {
     } else {
       $sAbout = hPOST("about");
     }
-
+    if (isset($_FILES['image_files']) && $_FILES['image_files']['name'] != '') {
+      $file = $_FILES['image_files'];
+    } else {
+      $file = NULL;
+    }
     //validate
     $sql = new SQLHelper;
     $bool = $sql->checkForDuplicate($sUser);
@@ -139,12 +143,80 @@ function register() {
       require 'register.php';
     } else {
       //store in db
-      $user = array($sUser, $sPass, $sFirst, $sLast, "student", $sAbout, $sEmail, NULL, $sResume, $sWebsite, 1, 0, date("Y-m-d"), date("Y-m-d"));
+      $user = array($sUser, $sPass, $sFirst, $sLast, "student", $sAbout, $sEmail, $file, $sResume, $sWebsite, 1, 0, date("Y-m-d"), date("Y-m-d"));
       $_SESSION['obj'] = $user;
       $_SESSION['user'] = $sUser;
       require 'setup.php';
     }
   }
+}
+
+function addProfile($user){
+   $uploadDirectory = '/profiles/' . $username . '/img/';
+   $file = $user[7];
+   try {
+       if ($file['name'] != '') {
+
+           $maxsize = 1000000;
+           $acceptable = array(
+               'image/jpeg',
+               'image/jpg',
+               'image/gif',
+               'image/png'
+           );
+           $errors = 0;
+           $fileName = $file['name'];
+           $fileSize = $file['size'];
+           $fileType = $file['type'];
+           $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+           $fileExtension = strtolower($fileExtension);
+           $fileTmp = $file['tmp_name'];
+           $fileDestination = $username . '_profile.' . $fileExtension;
+
+           if (!in_array($fileType, $acceptable) && !empty($fileType)) {
+               echo 'Invalid file type. Please upload an image.';
+               $errors++;
+               exit();
+           }
+
+           if ($fileSize >= $maxsize || $fileSize === 0) {
+               echo 'File must be under 1MB';
+               $errors++;
+               exit();
+           }
+
+           if($errors === 0){
+               if(!is_dir(SITE_ROOT . $uploadDirectory)){
+                   mkdir(SITE_ROOT . $uploadDirectory, 0755, true);
+               }
+               try{
+                   move_uploaded_file($fileTmp, SITE_ROOT . $uploadDirectory . $fileDestination);
+                   try{
+                       $commands = new SQLHelper();
+                       $commands->addUser($user);
+                   }
+                   catch(Exception $e){
+                       echo 'SQL Error';
+                   }
+                   echo 'Image has been uploaded succesfully. Profile has been updated.';
+               }
+               catch(Exception $e){
+                   'Upload failed';
+               }
+           }
+
+
+       } else if (empty($file)) {
+         $user[7] == null;
+         $commands = new SQLHelper();
+         $commands->addUser($user);
+       } else {
+         echo 'Failed';
+       }
+   }
+   catch(Exception $e){
+       echo 'Failed';
+   }
 }
 
 function setup() {
@@ -176,7 +248,7 @@ function setup() {
       $userObj = new User($userArray[0], $userArray[1], $userArray[2], $userArray[3], $userArray[4], $userArray[5], $userArray[6], $userArray[7], $userArray[8], $userArray[9], $userArray[10], $userArray[11], $userArray[12], $userArray[13]);
       $createDB->createDBUser($user, $sqlPass);
       $createFTP->createUser($user, $ftpPass);
-      $sql->addUser($userObj);
+      addProfile($userObj);
       $_SESSION['obj'] = NULL;
       $_SESSION['role'] = 1;
       after_successful_login();
