e32498e3c00f67e9ddd23cae7987348a2b3c76b3
Heather <dylanchoum123@yahoo.com>
2018-12-04 18:34:01 -0800

    did my team stuffs

--- team.php
+++ team.php
@@ -39,12 +39,12 @@
     </div>
     <div class='col-xs-12 col-sm-6 col-xl-3'>
       <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="./img/default-profile.jpg" alt="Card image cap">
+        <img class="card-img-top" src="./img/heather_team.jpg" alt="Card image cap">
         <div class="card-body">
           <h3 class="card-title">Heather Tran</h3>
-          <h5>Scrumaster / Front-End Developer</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary" target='_blank'>LinkedIn</a>
+          <h5>Scrum Master / Front-End Developer</h5>
+          <p class="card-text">As Scrum Master, Heather was responsible for dividing work and tracking the progress made on the project. She also was in charge of designing the modern UI as well as working on connecting the back-end of the login and register with the front-end</p>
+          <a href="https://www.linkedin.com/in/heather-tran-97092a108/" class="btn btn-primary" target='_blank'>LinkedIn</a>
         </div>
       </div>
     </div>
@@ -54,7 +54,7 @@
         <div class="card-body">
           <h3 class="card-title">Justin Crest</h3>
           <h5>Database Developer / Quality Assurance</h5>
-          <p class="card-text">As one of the developers, Justin was responsible for back-end development, and nearly all of the queries that this website runs on. 
+          <p class="card-text">As one of the developers, Justin was responsible for back-end development, and nearly all of the queries that this website runs on.
               Along with this, Justin was responsible for bug testing and quality assurance of both front-end and back-end changes up until the first public implementation of the website.</p>
           <a href="https://www.linkedin.com/in/justin-crest-a1a679aa/" class="btn btn-primary" target='_blank'>LinkedIn</a>
         </div>
